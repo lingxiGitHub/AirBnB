8221062048651cfe88a2cf1b22b7a562f37a3d92
lingxiGitHub <lingxi2015fall@gmail.com>
2023-01-13 13:41:43 -0800

    auth phase 3 logout

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -62,6 +62,17 @@ export const signup = (user) => async (dispatch) => {
 };
 // ...
 
+// frontend/src/store/session.js
+// ...
+export const logout = () => async (dispatch) => {
+    const response = await csrfFetch('/api/session', {
+        method: 'DELETE',
+    });
+    dispatch(removeUser());
+    return response;
+};
+// ...
+
 
 const initialState = { user: null };
 
