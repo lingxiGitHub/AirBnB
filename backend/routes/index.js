a7924a52ab236b3b4f60daa21dc23107a1531c81
lingxiGitHub <lingxi2015fall@gmail.com>
2023-03-21 19:48:29 -0700

    add back db

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,18 @@
 // backend/routes/index.js
 const express = require('express');
 const router = express.Router();
+
+//for testing
+
+// router.get('/hello/world', function (req, res) {
+//     res.cookie('XSRF-TOKEN', req.csrfToken());
+//     res.send('Hello World!');
+// });
+// router.post('/test', function (req, res) {
+//     res.json({ requestBody: req.body });
+// });
+
+
 const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
